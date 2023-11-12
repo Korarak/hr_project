e4ac836e533230f1702ec9abbf64a993f9b070a6
Korarak <irazor191119@gmail.com>
2023-11-13 01:17:27 +0700

    fix white noise again again

--- hr_project/urls.py
+++ hr_project/urls.py
@@ -5,5 +5,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('hr_app.urls')),
-]
-
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
