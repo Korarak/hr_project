bb799b279de5149da1f6b830908017c42f98d7ce
Korarak <irazor191119@gmail.com>
2023-11-13 09:23:14 +0700

    edit setting

--- hr_app/models.py
+++ hr_app/models.py
@@ -6,6 +6,6 @@ class Person(models.Model):
     p_lastname = models.CharField(max_length=50)
     p_tel = models.CharField(max_length=20,null=True)
     p_regisyear = models.DateField(null=True)
-    p_picture = models.ImageField(upload_to='profile_pictures/')
+    p_picture = models.ImageField(upload_to='images/')
     def __str__(self):
         return f'{self.p_firstname} {self.p_lastname}'
--- hr_app/urls.py
+++ hr_app/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('list',views.list),
     path('login',views.custom_login),
     path('logout',views.logout_view),
+    
 ]
